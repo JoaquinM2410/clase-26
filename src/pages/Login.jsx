ba3fb779dba18f781ba5c63f6e6a187b3d61e10e
JoaquinM2410 <joaquinmanrresa81@gmail.com>
2025-08-11 13:16:35 -0300

    add: contenido estatico en login, creacio ndel form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,22 @@ import { Layout } from "../components/Layout"
 const Login = () => {
   return (
     <Layout>
-      <p>Hola desde Login</p>
+      <h1>Inicia Sesion</h1>
+
+      <section>
+        <h2> Hola, bienvenido de nuevo</h2>
+        <form>
+          <div>
+            <label>Correo electronico</label>
+            <input type="email" name="email" />
+          </div>
+          <div>
+            <label>Contraseña</label>
+            <input type="password" name="contraseña" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }
