7f4f0977e1a776e85a105f129b1de36f225ec9f2
JoaquinM2410 <joaquinmanrresa81@gmail.com>
2025-08-11 13:25:37 -0300

    agg: contenido estatico a notfound

--- src/pages/NoteFound.jsx
+++ src/pages/NoteFound.jsx
@@ -3,7 +3,10 @@ import { Layout } from "../components/Layout"
 const NoteFound = () => {
   return (
     <Layout>
-      <p>Hola desde NoteFound</p>
+      <h1>404</h1>
+      <h2>Página no encontrada</h2>
+      <p>Lo sentimos, la página que buscas no existe o ha sido movida.</p>
+      <Link to="/">Volver al inicio </Link>
     </Layout>
   )
 }
